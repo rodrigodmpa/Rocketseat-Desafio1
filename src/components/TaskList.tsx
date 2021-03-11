45a4b6aa1e58502f1e84f9c78eac6b04401900bf
Rodrigo Demetrio Palma <rodrigo.demetrio.palma@gmail.com>
2021-03-11 20:47:01 -0300

    desafio 1

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,34 @@ interface Task {
   isComplete: boolean;
 }
 
+function getRandomArbitrary(min: number, max: number) {
+  return Math.random() * (max - min) + min;
+}
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const id = getRandomArbitrary(1,10000);
+    const newTask = {title: newTaskTitle, isComplete: false, id };
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((task) => task.id !== id)
+    setTasks(newTasks);
   }
 
   return (
